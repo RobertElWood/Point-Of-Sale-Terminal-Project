1ebe93308354bbcbd1eb3d8a143d6dc763e32c99
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-10-18 10:15:58 -0400

    Fixed payment methods

--- Program.cs
+++ Program.cs
@@ -7,32 +7,46 @@ namespace Point_of_Sale_Terminal
 
         static void Main(string[] args)
         {
-
-            //Bool to prompt loop
+            //Bool to prompt Shopping Again loop
+            bool shopAgain = true;
+            //Bool to prompt asking to continue loop
             bool runAgain = true;
-            //New instance of POS class
-            POS storeVisit = new POS();
-            //Top Console Greeting
-            Logo Beauty = new Logo();            
-
-
-            //Main store visit loop
-            while (runAgain)
+                   
+            
+            while (shopAgain)
             {
-                //Item List header
-                Beauty.Logo2();
-                //Printing actual product inventory
-                storeVisit.PrintInventory();
-                Console.WriteLine();//Formatting
-                //Calling Purchase method to inquire with the user what they'd like to purchase TODO: Add something that says the user can just exit right away maybe?
-                storeVisit.Purchase();
-                Console.WriteLine();//Formatting
-                //Prompt user to go again
-                runAgain = AskToContinue();
+                //New instance of POS class
+                POS storeVisit = new POS();
+                //Top Console Greeting
+                Logo Beauty = new Logo();
+
+                //Main store visit loop
+                while (runAgain)
+                {
+                    //Item List header
+                    Beauty.Logo2();
+                    
+                    //Printing actual product inventory
+                    storeVisit.PrintInventory();
+                    Console.WriteLine();//Formatting
+                   
+                    //Calling Purchase method to inquire with the user what they'd like to purchase
+                    storeVisit.Purchase();
+                    Console.WriteLine();//Formatting
+                   
+                    //Prompt user to go again
+                    runAgain = AskToContinue();
+                }
+                
+                //Prompt user to select their pay type if they're done browsing
+                storeVisit.ChoosePaymentMethod();
+                storeVisit.PrintReciept();
+
+                shopAgain = ShopAgain();
+                runAgain = true;
             }
-            //Prompt user to select their pay type if they're done browsing
-            storeVisit.ChoosePaymentMethod();
         }
+
 
 
         //Method to prompt user to continue
@@ -48,7 +62,7 @@ namespace Point_of_Sale_Terminal
             }
             else if (input == "n")
             {
-                Console.WriteLine("Thank you for stopping by!");
+                Console.WriteLine("\nGreat, lets get you checked out!");
                 return false;
             }
             else
@@ -56,6 +70,30 @@ namespace Point_of_Sale_Terminal
                 Console.WriteLine("I didn't catch that.");
                 return AskToContinue();
             }
+        }
+
+        //Method to prompt user if they want to shop again
+        public static bool ShopAgain()
+        {
+            Console.WriteLine("Would you like to shop again? y/n");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "y")
+            {
+                Console.WriteLine("Okay, great!");
+                return true;
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("\nThank you for shopping at Retro Boys, the only video game store endorsed by Samuel L. Jackson, MF!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("I didn't catch that.");
+                return AskToContinue();
+            }
+
         }
 
     }
